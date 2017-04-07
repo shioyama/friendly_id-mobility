1e626a3e8dc72fc6e7ca9047e45de16a619b11bc
Chris Salzberg <chris@dejimata.com>
2017-04-07 23:31:25 +0900

    Add code

--- lib/friendly_id/mobility.rb
+++ lib/friendly_id/mobility.rb
@@ -1,7 +1,31 @@
+require "friendly_id"
 require "friendly_id/mobility/version"
 
 module FriendlyId
   module Mobility
-    # Your code goes here...
+    class << self
+      def setup(model_class)
+        model_class.friendly_id_config.use :slugged
+      end
+
+      def included(model_class)
+        advise_against_untranslated_model(model_class)
+
+        mod = Module.new do
+          def friendly
+            super.i18n
+          end
+        end
+        model_class.send :extend, mod
+      end
+
+      def advise_against_untranslated_model(model)
+        field = model.friendly_id_config.query_field
+        if !model.respond_to?(:translated_attribute_names) || model.translated_attribute_names.exclude?(field)
+          raise "[FriendlyId] You need to translate the '#{field}' field with " \
+            "Mobility (add 'translates :#{field}' in your model '#{model.name}')"
+        end
+      end
+    end
   end
 end
